edf40a9b4df5a7f3e050052c3e8bdbd184126c9b
Brian Clark <brian@clark.zone>
2016-08-28 11:57:24 -0400

    first commit

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe service('mongod') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(27_017) do
+  it { should be_listening.with('tcp') }
+end
+
+describe file('/etc/mongod.conf') do
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe command('mongod --version') do
+  its(:stdout) { should include 'db version v3.2' }
+end
+
+describe file('/etc/logrotate.d/mongod') do
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
