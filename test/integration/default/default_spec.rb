af4ccef2b4d8d59058c9253492a2c6511690776e
Brian Clark <brian@clark.zone>
2018-03-27 13:05:01 -0400

    refactor how repo key is managed

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -11,7 +11,7 @@ end
 
 describe package('mongodb-org') do
   it { should be_installed }
-  its('version') { should match /^3.2.10/ }
+  its('version') { should match /^3.4.6/ }
 end
 
 describe service('mongod') do
