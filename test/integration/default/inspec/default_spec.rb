31c91f20f3065b0cc81219cc502d0b51728e75ad
Brian Clark <brian@clark.zone>
2019-02-27 11:27:24 -0500

    add processManagement.timeZoneInfo to config for 3.6+

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -30,6 +30,17 @@ describe file('/etc/mongod.conf') do
   its('mode') { should cmp '0644' }
 end
 
+describe yaml('/etc/mongod.conf') do
+  its(%w(net port)) { should eq 27_017 }
+  its(%w(net bindIp)) { should eq '127.0.0.1' }
+  its(%w(storage dbPath)) { should eq data_dir }
+  its(%w(storage journal enabled)) { should eq true }
+  its(%w(systemLog destination)) { should eq 'file' }
+  its(%w(systemLog logAppend)) { should eq true }
+  its(%w(systemLog path)) { should eq '/var/log/mongodb/mongod.log' }
+  its(%w(processManagement timeZoneInfo)) { should eq '/usr/share/zoneinfo' }
+end
+
 describe directory(data_dir) do
   it { should be_owned_by mongodb_user }
   it { should be_grouped_into mongodb_group }
