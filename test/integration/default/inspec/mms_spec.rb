872e38e127c28650879a0adf15b04dacd54f4d60
Brian Clark <brian@clark.zone>
2019-02-27 09:41:46 -0500

    update mms agent configuration and tests

---
+++ test/integration/default/inspec/mms_spec.rb
@@ -0,0 +1,37 @@
+mongodb_user = os.debian? ? 'mongodb' : 'mongod'
+
+describe package('mongodb-mms-automation-agent-manager') do
+  it { should be_installed }
+end
+
+describe service('mongodb-mms-automation-agent') do
+  it { should be_installed }
+  it { should be_enabled }
+end
+
+describe file('/etc/mongodb-mms/automation-agent.config') do
+  it { should be_file }
+  it { should be_owned_by mongodb_user }
+  it { should be_grouped_into mongodb_user }
+  its('mode') { should cmp '0600' }
+  its('content') { should match /^mmsGroupId=my_automation_agent_group_id$/ }
+  its('content') { should match /^mmsApiKey=my_automation_agent_api_key$/ }
+end
+
+describe package('mongodb-mms-monitoring-agent') do
+  it { should be_installed }
+end
+
+describe service('mongodb-mms-monitoring-agent') do
+  it { should be_installed }
+  it { should be_enabled }
+end
+
+describe file('/etc/mongodb-mms/monitoring-agent.config') do
+  it { should be_file }
+  it { should be_owned_by 'mongodb-mms-agent' }
+  it { should be_grouped_into 'mongodb-mms-agent' }
+  its('mode') { should cmp '0600' }
+  its('content') { should match /^mmsGroupId=my_monitor_agent_group_id$/ }
+  its('content') { should match /^mmsApiKey=my_monitor_agent_api_key$/ }
+end
--- test/integration/v40/inspec/default_spec.rb
+++ test/integration/v40/inspec/default_spec.rb
@@ -13,3 +13,11 @@ describe port(27_017) do
   it { should be_listening }
   its('addresses') { should include '127.0.0.1' }
 end
+
+describe package('mongodb-mms-automation-agent-manager') do
+  it { should_not be_installed }
+end
+
+describe package('mongodb-mms-monitoring-agent') do
+  it { should_not be_installed }
+end
